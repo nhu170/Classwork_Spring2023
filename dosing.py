67694c6e948262b47982b285750e2775db727c6b
Nan Hu <olina19980108@gmail.com>
2023-01-27 12:54:13 -0500

    Modified modularity of the code

--- dosing.py
+++ dosing.py
@@ -5,6 +5,50 @@
              type of medical treatment or diagnostics.
 """
 
+def diagnosis_input():
+    print("Day One Dosing Guidelines")
+    print("")
+    print("Choose diagnosis:")
+    print("1 - Acute otitis media")
+    print("2 - Acute bacterial sinusitis")
+    print("3 - Community-acquired pneumonia")
+    print("4 - Pharyngitis/tonsilitis")
+    diagnosis = int(input("Enter a number: "))
+    return diagnosis
+  
+# Can separate this into weight_input and weight_parse to process data
+def weight_input():
+    print("PATIENT WEIGHT")
+    print("Enter patient weight followed by units of kg or lb.")
+    print("Examples:  65.3 lb      21.0 kg")
+    weight_input = input("Enter weight: ")
+    weight_data = weight_input.split(" ")
+    weight = float(weight_data[0])
+    units = weight_data[1]
+    if units == "lb":
+        weight = weight / 2.205
+    return weight
+
+def dose_analysis(diagnosis, weight):
+    dosages_mg_per_kg = [30, 10, 10, 12]
+    dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
+    dosage_analy = weight * dosage_mg_per_kg
+    return dosage_analy
+
+def dose_output(weight, dosage_analy):
+    print("CORRECT DOSAGE")
+    print("For a patient weighing {:.1f} kg,".format(weight))
+    print("  the correct dosage is {:.1f} mg the first day"
+          .format(dosage_analy))
+
+# Driver
+if __name__ == '__main__':
+    dia_in = diagnosis_input() 
+    wei_in = weight_input()
+    dose_analy = dose_analysis(dia_in, wei_in)
+    dose_output(wei_in, dose_analy)
+
+
 
 def dose_amount():
     print("Day One Dosing Guidelines")
