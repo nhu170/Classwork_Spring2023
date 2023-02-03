102a3ebecc0a520c7632c56e7b42fcea029daed3
Nan Hu <olina19980108@gmail.com>
2023-02-03 13:13:28 -0500

    added function to check fever na dincluded example of writing unit test before function

---
+++ check_fever.py
@@ -0,0 +1,15 @@
+# Sometimes can write tests b/4 writing functions
+
+def test_check_fever():
+    from blood_calculator import check_fever
+    input_temp = [97, 98.6, 100.1, 103]
+    answer = check_fever(input_temp)
+    expected = True
+    assert answer == expected
+
+def check_fever(input_list):
+    for temperature in input_list:
+        if temperature > 100.5:
+            return True
+    return False
+
