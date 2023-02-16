c23a598313c84436961fd6b7bc3f54dc8e23b7e0
Nan Hu <olina19980108@gmail.com>
2023-02-16 11:41:30 -0500

    Made an error and fixed it

---
+++ error.py
@@ -0,0 +1,19 @@
+def calc(a):
+    if type(a) is str:
+        raise TypeError("Cannot input string")
+    ans = a**2
+    return a
+
+
+def main():
+    x = "2"
+    try:
+        ans = calc(x)
+    except TypeError:
+        new_x = int(x)
+        ans = calc(new_x)
+    print(ans)
+
+
+if __name__== "__main__":
+    main()
